017bb59f3fb43cb67bd22fe72427aac22e17332a
Shir Markowitz <shirm91799@gmail.com>
2023-10-29 23:43:52 -0400

    creating searchform

---
+++ src/Components/SearchForm.js
@@ -0,0 +1,9 @@
+import TextField from '@mui/material/TextField';
+
+const SearchForm = () => {
+    return (
+        <TextField sx={{marginTop: 5, width: 400}} id="outlined-basic" label="Outlined" variant="outlined" />
+     );
+}
+
+export default SearchForm;
