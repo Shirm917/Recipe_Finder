53e0c4241b3cf520f52f2fc1e9427d6458a91794
Shir Markowitz <shirm91799@gmail.com>
2023-10-30 01:23:08 -0400

    adding onChange to searchform, dispatching setSearchText

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,14 +1,19 @@
+import { useDispatch } from "react-redux";
+import { setSearchText } from "../redux/slices/recipeSlice";
 import TextField from "@mui/material/TextField";
-import InputAdornment from '@mui/material/InputAdornment';
-import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchForm = () => {
+  const dispatch = useDispatch();
+
   return (
     <TextField
       sx={{ width: "100%", backgroundColor: "#fff", borderRadius: "10px" }}
       id="outlined-basic"
       placeholder="Enter Ingredients..."
       variant="outlined"
+      onChange={(event) => dispatch(setSearchText(event.target.value))}
       InputProps={{
         endAdornment: (
           <InputAdornment position="end">
