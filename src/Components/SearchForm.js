29e0081b75ac072d3ccfc69af2e3a19f1a993bbe
Shir Markowitz <shirm91799@gmail.com>
2023-11-01 23:58:36 -0400

    adding form element around the text input and making the search icon a button

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -3,25 +3,34 @@ import { setSearchText } from "../redux/slices/recipeSlice";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
+import IconButton from "@mui/material/IconButton";
 
 const SearchForm = () => {
   const dispatch = useDispatch();
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <TextField
-      sx={{ width: "100%", backgroundColor: "#fff", borderRadius: "10px" }}
-      id="outlined-basic"
-      placeholder="Enter Ingredients..."
-      variant="outlined"
-      onChange={(event) => dispatch(setSearchText(event.target.value))}
-      InputProps={{
-        endAdornment: (
-          <InputAdornment position="end">
-            <SearchIcon />
-          </InputAdornment>
-        ),
-      }}
-    />
+    <form className="search-form" onSubmit={handleSubmit}>
+      <TextField
+        sx={{ width: "100%", backgroundColor: "#fff", borderRadius: "10px" }}
+        id="outlined-basic"
+        placeholder="Enter Ingredients..."
+        variant="outlined"
+        onChange={(event) => dispatch(setSearchText(event.target.value))}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton type="submit">
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </form>
   );
 };
 
