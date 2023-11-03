c6310f95cdef06441e3554478c912db2158948a4
Shir Markowitz <shirm91799@gmail.com>
2023-11-02 23:43:58 -0400

    fixing some .env variables and creating my redux thunk folder for my fetchRecipes function

---
+++ src/redux/thunks/fetchRecipes.js
@@ -0,0 +1,13 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchRecipes = createAsyncThunk(
+    "recipe/fetchRecipes",
+    async (searchText) => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}?type=public&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&q=${searchText}`);
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+);
