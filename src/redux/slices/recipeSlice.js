5c3c92a12fffe4ce306aac1330f4a7b850b8b542
Shir Markowitz <shirm91799@gmail.com>
2023-11-03 00:30:03 -0400

    adding extra reducers for fetchRecipes

--- src/redux/slices/recipeSlice.js
+++ src/redux/slices/recipeSlice.js
@@ -1,18 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchRecipes } from "../thunks/fetchRecipes";
 
 const initialState = {
-    searchText: "",
-    recipes: [],
+  searchText: "",
+  recipes: [],
+  isLoading: false,
+  error: null
 };
 
 export const recipeSlice = createSlice({
-    name: "recipe",
-    initialState,
-    reducers: {
-        setSearchText: (state,action) => {
-            state.searchText = action.payload;
-        }
-    }
+  name: "recipe",
+  initialState,
+  reducers: {
+    setSearchText: (state, action) => {
+      state.searchText = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchRecipes.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchRecipes.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.recipes = action.payload;
+    },
+    [fetchRecipes.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
+    },
+  },
 });
 
 export const { setSearchText } = recipeSlice.actions;
