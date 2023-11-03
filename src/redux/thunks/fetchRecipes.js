1c9bfec017d24e39c5ec2bcdf35653bbf79e5735
Shir Markowitz <shirm91799@gmail.com>
2023-11-03 00:30:39 -0400

    returning recipe objects in fetch recipes

--- src/redux/thunks/fetchRecipes.js
+++ src/redux/thunks/fetchRecipes.js
@@ -5,7 +5,8 @@ export const fetchRecipes = createAsyncThunk(
     async (searchText) => {
         try {
             const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}?type=public&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&q=${searchText}`);
-            console.log(response);
+            const data = await response.json();
+            return data.hits;
         } catch (err) {
             console.log(err);
         }
