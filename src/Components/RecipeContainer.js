f266aca0bc59a606702b07912d25d172b7d1f670
Shir Markowitz <shirm91799@gmail.com>
2023-11-04 22:57:05 -0400

    adding recipe cards to recipe container

--- src/Components/RecipeContainer.js
+++ src/Components/RecipeContainer.js
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux";
+import RecipeCard from "./RecipeCard";
+
 const RecipeContainer = () => {
+  const recipes = useSelector((state) => state.recipe.recipes);
   return (
     <section className="recipe-container">
-        
+      {recipes &&
+        recipes.map((recipe) => {
+          return <RecipeCard key={recipe.recipe.uri} recipe={recipe.recipe} />;
+        })}
     </section>
   );
 };
