ecde69f9b431c537d56f131b2ed1a0bf70ed7c77
Shir Markowitz <shirm91799@gmail.com>
2023-11-06 00:10:56 -0500

    creating recipe details component

--- src/Components/RecipeDetails.js
+++ src/Components/RecipeDetails.js
@@ -1,24 +1,21 @@
 import { useSelector } from "react-redux";
 
 const RecipeDetails = () => {
-  const chosenRecipe = (state) => state.recipe.chosenRecipe;
+  const chosenRecipe = useSelector((state) => state.recipe.chosenRecipe);
   const {
     label,
     image,
     calories,
     cuisineType,
-    dietLabels,
     ingredientLines,
-    mealType,
   } = chosenRecipe;
+
   return (
-    <section>
+    <section className="recipe-details">
       <h1>{label}</h1>
       <img src={image} alt={label} />
       <p>Calories: {calories.toFixed(2)}</p>
-      <p>Cuisine Type: {cuisineType}</p>
-      <p>Diet Labels: {dietLabels}</p>
-      <p>Meal Type: {mealType}</p>
+      <p>Cuisine Type: {cuisineType || "Unknown"}</p>
       <ul>
         {ingredientLines.map((ingredient, index) => {
           return <li key={index}>{ingredient}</li>;
