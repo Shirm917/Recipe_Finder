e2cdc2954b2818e8b34294fcbf96264c54dfe0b8
Shir Markowitz <shirm91799@gmail.com>
2023-11-07 23:55:20 -0500

    adding isLoading tenary operator to recipe container

--- src/Components/RecipeContainer.js
+++ src/Components/RecipeContainer.js
@@ -1,9 +1,16 @@
 import { useSelector } from "react-redux";
 import RecipeCard from "./RecipeCard";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const RecipeContainer = () => {
   const recipes = useSelector((state) => state.recipe.recipes);
-  return (
+  const isLoading = useSelector((state) => state.recipe.isLoading);
+  return isLoading ? (
+    <section className="loading-screen">
+      <h1>Loading</h1>
+      <CircularProgress color="inherit" />
+    </section>
+  ) : (
     <section className="recipe-container">
       {recipes &&
         recipes.map((recipe) => {
