4ba017ccdbb1512f383e4856d17ab7ac7f4ef0ec
Shir Markowitz <shirm91799@gmail.com>
2023-11-08 23:56:21 -0500

    adding isError tenary operator and adding style to it

--- src/Components/RecipeContainer.js
+++ src/Components/RecipeContainer.js
@@ -5,10 +5,15 @@ import CircularProgress from "@mui/material/CircularProgress";
 const RecipeContainer = () => {
   const recipes = useSelector((state) => state.recipe.recipes);
   const isLoading = useSelector((state) => state.recipe.isLoading);
+  const isError = useSelector((state) => state.recipe.error);
   return isLoading ? (
     <section className="loading-screen">
       <h1>Loading</h1>
       <CircularProgress color="inherit" />
+    </section>
+  ) : isError ? (
+    <section className="error-screen">
+      <h1>{isError}</h1>
     </section>
   ) : (
     <section className="recipe-container">
