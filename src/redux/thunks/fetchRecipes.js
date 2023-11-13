075df5ed1e08105d720ed83124dd4d4530d1270a
Shir Markowitz <shirm91799@gmail.com>
2023-11-13 17:38:03 -0500

    changing fetch recipes to include filter options

--- src/redux/thunks/fetchRecipes.js
+++ src/redux/thunks/fetchRecipes.js
@@ -2,11 +2,18 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchRecipes = createAsyncThunk(
   "recipe/fetchRecipes",
-  async (searchText) => {
-    const response = await fetch(
-      `${process.env.REACT_APP_API_BASE_URL}?type=public&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&q=${searchText}`
-    );
+  async ({searchText, filterOptions}) => {
+    const { mealType, dishType, calories, diet } = filterOptions;
+    const caloriesNumber = parseInt(calories);
+    let url = `${process.env.REACT_APP_API_BASE_URL}?type=public&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&q=${searchText}`;
+    console.log(mealType);
+    if (mealType) url += `&mealType=${mealType}`;
+    if (dishType) url += `&dishType=${dishType}`;
+    if (calories) url += `&calories=${caloriesNumber}`;
+    if (diet) url += `&diet=${diet}`;
+    const response = await fetch(url);
     const data = await response.json();
+    console.log(data.hits);
     return data.hits;
   }
 );
