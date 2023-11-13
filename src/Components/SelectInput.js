07e5af2a437644be56fad823365eea49a121b36c
Shir Markowitz <shirm91799@gmail.com>
2023-11-13 17:36:30 -0500

    creating select input

---
+++ src/Components/SelectInput.js
@@ -0,0 +1,32 @@
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+
+const SelectInput = ({ value, handleFilterChange, label, menuItemArr, id, propertyKey }) => {
+  const handleChange = (event) => {
+    handleFilterChange(propertyKey, event.target.value);
+  };
+
+  return (
+    <Box sx={{width: 120, marginTop: 1}}>
+      <FormControl fullWidth>
+        <InputLabel id={`${id}-label`}>{label}</InputLabel>
+        <Select
+          labelId={`${id}-label`}
+          id={id}
+          value={value}
+          label={label}
+          onChange={handleChange}
+        >
+          {menuItemArr.map((item) => (
+            <MenuItem value={item}>{item}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default SelectInput;
