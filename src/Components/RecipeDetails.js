223f9b23e4911397c66cb4a8d46b32609869a72f
Shir Markowitz <shirm91799@gmail.com>
2023-11-18 22:51:44 -0500

    adding serving amount and calories per serving to recipe details card

--- src/Components/RecipeDetails.js
+++ src/Components/RecipeDetails.js
@@ -3,7 +3,16 @@ import BackArrow from "./BackArrow";
 
 const RecipeDetails = () => {
   const chosenRecipe = useSelector((state) => state.recipe.chosenRecipe);
-  const { label, image, calories, cuisineType, ingredientLines } = chosenRecipe;
+  const {
+    label,
+    image,
+    calories,
+    cuisineType,
+    ingredientLines,
+    yield: servings,
+  } = chosenRecipe;
+
+  const caloriesPerServing = (calories / servings).toFixed();
 
   return (
     <section className="recipe-details-container">
@@ -11,7 +20,8 @@ const RecipeDetails = () => {
       <div className="recipe-details">
         <h1>{label}</h1>
         <img src={image} alt={label} />
-        <p>Calories: {calories.toFixed()}</p>
+        <p>{servings} Servings</p>
+        <p>Calories Per Serving: {caloriesPerServing}</p>
         <p className="cuisine-type">Cuisine Type: {cuisineType || "Unknown"}</p>
         <div className="ingredient-lines-container">
           <ul>
