40cb8bdfaf663808287a8bebe8ad37358be453f3
Shir Markowitz <shirm91799@gmail.com>
2023-11-18 22:34:44 -0500

    added calories and calories conditional to recipes fetch

--- src/redux/thunks/fetchRecipes.js
+++ src/redux/thunks/fetchRecipes.js
@@ -3,8 +3,10 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const fetchRecipes = createAsyncThunk(
   "recipe/fetchRecipes",
   async ({searchText, filterOptions}) => {
-    const { mealType, dishType, diet } = filterOptions;
-    let url = `${process.env.REACT_APP_API_BASE_URL}?type=public&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&q=${searchText}`;
+    const { maxCalories, mealType, dishType, diet } = filterOptions;
+    let url = `${process.env.REACT_APP_API_BASE_URL}?type=public&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&q=${searchText}&count=60`;
+    const maxCaloriesNum = Number(maxCalories);
+    if (maxCalories) url += `&calories=0-${maxCaloriesNum}`;
     if (mealType) url += `&mealType=${mealType}`;
     if (dishType) url += `&dishType=${dishType}`;
     if (diet) url += `&diet=${diet}`;
