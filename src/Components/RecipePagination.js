6e226913246dc195e0d31e436e8e76f82b97e32a
Shir Markowitz <shirm91799@gmail.com>
2023-12-18 23:38:07 -0500

    adding handlePrevPage and handleNextPage

--- src/Components/RecipePagination.js
+++ src/Components/RecipePagination.js
@@ -4,35 +4,39 @@ import IconButton from "@mui/material/IconButton";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import Typography from "@mui/material/Typography";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 
 const RecipePagination = () => {
   const { currentPage, recipes } = useSelector((state) => state.recipe);
   const dispatch = useDispatch();
 
-  const itemsPerPage = 10;
-
   const getPaginationedItems = (page) => {
-    const startIndex = (page - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const recipesCopy = [...recipes];
-    const currentRecipes = recipesCopy.slice(startIndex, endIndex);
+    const currentRecipes = recipes[page - 1].hits;
     return currentRecipes;
   };
 
-  const handlePageChange = (event, newPage) => {
-    dispatch(setCurrentPage(newPage));
-    const currentRecipes = getPaginationedItems(newPage);
-    dispatch(setCurrentRecipes(currentRecipes));
+  const handlePrevPage = () => {
+    if (currentPage === 1) return;
+    const prevPage = currentPage - 1;
+    dispatch(setCurrentPage(currentPage - 1));
+  };
+
+  const handleNextPage = () => {
+    const nextPage = currentPage + 1;
+    dispatch(setCurrentPage(nextPage));
+    if (recipes[nextPage - 1]) {
+      const currentRecipes = getPaginationedItems(nextPage);
+      dispatch(setCurrentRecipes(currentRecipes));
+    }
   };
 
   return (
-    <Box sx={{display: "flex", alignItems: "center"}}>
-      <IconButton>
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <IconButton onClick={handlePrevPage}>
         <ChevronLeftIcon />
       </IconButton>
       <Typography variant="body1">{currentPage}</Typography>
-      <IconButton>
+      <IconButton onClick={handleNextPage}>
         <ChevronRightIcon />
       </IconButton>
     </Box>
