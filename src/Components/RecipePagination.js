b3f5cb14afa13497d357387ff170bd6376a14a49
Shir Markowitz <shirm91799@gmail.com>
2023-12-20 03:24:06 -0500

    added return statement for handleNextPage for pagesNeeded conditional and added condtional for the disabling of the forward arrow and back arrow

--- src/Components/RecipePagination.js
+++ src/Components/RecipePagination.js
@@ -6,6 +6,7 @@ import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { current } from "@reduxjs/toolkit";
 
 const RecipePagination = () => {
   const { currentPage, recipes } = useSelector((state) => state.recipe);
@@ -16,6 +17,9 @@ const RecipePagination = () => {
     return currentRecipes;
   };
 
+  const backArrowDisabled = currentPage === 1;
+  const forwardArrowDisabled = currentPage === Math.ceil(recipes[0].count / 20);
+
   const handlePrevPage = () => {
     if (currentPage === 1) return;
     const prevPage = currentPage - 1;
@@ -23,10 +27,12 @@ const RecipePagination = () => {
     if (recipes[prevPage - 1]) {
       const currentRecipes = getPaginationedItems(prevPage);
       dispatch(setCurrentRecipes(currentRecipes));
-    };
+    }
   };
 
   const handleNextPage = () => {
+    const pagesNeeded = Math.ceil(recipes[0].count / 20);
+    if (currentPage === pagesNeeded) return;
     const nextPage = currentPage + 1;
     const url = recipes[currentPage - 1]._links.next.href;
     dispatch(setCurrentPage(nextPage));
@@ -40,11 +46,11 @@ const RecipePagination = () => {
 
   return (
     <Box sx={{ display: "flex", alignItems: "center", marginTop: "auto" }}>
-      <IconButton onClick={handlePrevPage}>
+      <IconButton onClick={handlePrevPage} disabled={backArrowDisabled}>
         <ChevronLeftIcon />
       </IconButton>
       <Typography variant="body1">{currentPage}</Typography>
-      <IconButton onClick={handleNextPage}>
+      <IconButton onClick={handleNextPage} disabled={forwardArrowDisabled}>
         <ChevronRightIcon />
       </IconButton>
     </Box>
